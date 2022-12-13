d50558e74a8e7f34d844f39f748e071518b7689f
fewtap9 <banansplit21@hotmail.com>
2022-12-13 14:46:53 -0300

    Added todos and bugs

--- FlightAPI/Controllers/FlightsController.cs
+++ FlightAPI/Controllers/FlightsController.cs
@@ -4,6 +4,7 @@ using MySql.Data.MySqlClient;
 using System.Data;
 using Newtonsoft.Json;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace FlightAPI.Controllers
 {
@@ -11,6 +12,7 @@ namespace FlightAPI.Controllers
     [Route("api/[controller]")]
     public class FlightsController : ControllerBase
     {
+        
 
         private readonly ILogger<FlightsController> _logger;
 
@@ -20,11 +22,12 @@ namespace FlightAPI.Controllers
         }
 
         [HttpGet("{date}")]
-        public string GetFlightsFromDatabase(DateTime date)
+        public ContentResult GetFlightsFromDatabase(DateTime date)
         {
             string connectionString = "server=127";
             // Connect to the MySQL database
-            connectionString = @"server=lin-13041-7784-mysql-primary.servers.linodedb.net;user id=linroot;password=7ZmXl9xm9J@qsBIZ;database=Departures";
+            connectionString =
+                @"server=lin-13041-7784-mysql-primary.servers.linodedb.net;user id=linroot;password=7ZmXl9xm9J@qsBIZ;database=Departures";
 
 
             using (var connection = new MySqlConnection(connectionString))
@@ -36,7 +39,7 @@ namespace FlightAPI.Controllers
                 catch (Exception ex)
                 {
 
-                    return "Error ... " + ex.ToString() + "\n StatusCode: 500, Internal Server Error" + "\n\n" + "Connection string: " + connectionString;
+                    return Content(ex.Message);
                 }
 
 
@@ -61,7 +64,9 @@ namespace FlightAPI.Controllers
                                 DepartureAirport = reader.GetString("DepartureAirport"),
                                 ArrivalAirport = reader.GetString("ArrivalAirport"),
                                 Planned = reader.GetDateTime("Planned"),
-                                Estimated = reader.IsDBNull("Estimated") ? (DateTime?)null : reader.GetDateTime("Estimated"),
+                                Estimated = reader.IsDBNull("Estimated")
+                                    ? (DateTime?)null
+                                    : reader.GetDateTime("Estimated"),
                                 status_kl = reader.GetString("status_kl"),
                                 status_en = reader.GetString("status_en"),
                                 status_da = reader.GetString("status_da"),
@@ -71,7 +76,7 @@ namespace FlightAPI.Controllers
                             };
 
                             flight.Planned = flight.Planned.AddHours(-3);
-                            if(flight.Estimated != null)
+                            if (flight.Estimated != null)
                             {
                                 flight.Estimated = flight.Estimated.Value.AddHours(-3);
                             }
@@ -79,35 +84,86 @@ namespace FlightAPI.Controllers
                             var properties = flight.GetType().GetProperties();
 
                             // Iterate over the array of properties
-                            foreach (var property in properties)
+                            /*foreach (var property in properties)
                             {
                                 // Get the value of the property
                                 var value = property.GetValue(flight);
 
                                 // Print the property name and value
                                 Console.WriteLine($"{property.Name}: {value}");
-                            }
+                            }*/
+
                             flights.Add(flight);
-                            
+
                         }
                     }
                 }
+
                 connection.Close();
                 JsonSerializerSettings settings = new JsonSerializerSettings();
                 settings.NullValueHandling = NullValueHandling.Include;
-                return JsonConvert.SerializeObject(flights, settings);
-            }
-            
+                string json = JsonConvert.SerializeObject(flights, Formatting.None, settings);
+                json.Trim();
 
-            
+                return Content(json, "application/json");
+            }
+
+
+
         }
 
+        //TODO: Make sure that the json is being received correctly
+        //BUG : The json is not being received correctly, it's sending a 400 bad request
+        [HttpPost]
+        [Route("/rooms/add")]
+        public string PostRoom([FromBody]string json)
+        {
+            List<Room> rooms;
+            // Create the connection string
+            string connectionString =
+                "server=lin-13041-7784-mysql-primary.servers.linodedb.net;user id=linroot;password=7ZmXl9xm9J@qsBIZ;database=Departures";
+            try
+            {
+                 rooms = JsonConvert.DeserializeObject<List<Room>>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "Error";
+            }
+            
+            // Open the connection
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                foreach (var room in rooms)
+                {
+                    // Build the SQL query
+                    string query = "INSERT INTO Rooms (RoomNumber, FlightHash) VALUES (@roomnumber, @hash)";
 
-        [HttpPost("{Hash}-{roomnumber}")]
-        public void PostRoom(string Hash, string roomnumber)
+                    // Execute the query and retrieve the flights
+                    using (var command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@roomnumber", room.RoomNumber);
+                        command.Parameters.AddWithValue("@hash", room.FlightHash);
+
+                        command.ExecuteNonQuery();
+                        Console.WriteLine(room.RoomNumber + " Inserted");
+                    }
+                }
+                connection.Close();
+                
+            }
+
+            return "Success";
+        }
+
+        [HttpDelete("{hash}-{roomnumber}")]
+        public void DeleteRoom(string hash, string roomnumber)
         {
             // Create the connection string
-            string connectionString = "server=lin-13041-7784-mysql-primary.servers.linodedb.net;user id=linroot;password=7ZmXl9xm9J@qsBIZ;database=Departures";
+            string connectionString =
+                "server=lin-13041-7784-mysql-primary.servers.linodedb.net;user id=linroot;password=7ZmXl9xm9J@qsBIZ;database=Departures";
 
             // Open the connection
             using (MySqlConnection connection = new MySqlConnection(connectionString))
@@ -115,13 +171,13 @@ namespace FlightAPI.Controllers
                 connection.Open();
 
                 // Create the INSERT INTO statement
-                string query = "INSERT INTO Rooms (FlightHash, RoomNumber) VALUES (@Hash, @roomnumber)";
+                string query = "DELETE FROM Rooms WHERE FlightHash = @hash AND RoomNumber = @roomnumber";
 
                 // Create a command using the query and the connection
                 using (MySqlCommand command = new MySqlCommand(query, connection))
                 {
                     // Add the parameters and their values to the command
-                    command.Parameters.AddWithValue("@Hash", Hash);
+                    command.Parameters.AddWithValue("@Hash", hash);
                     command.Parameters.AddWithValue("@roomnumber", roomnumber);
 
                     // Execute the query
@@ -130,12 +186,10 @@ namespace FlightAPI.Controllers
             }
 
 
+
         }
-        
-        
+
 
     }
-
-
 }
 
